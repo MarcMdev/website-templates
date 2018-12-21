b50ba6b42302269c089f5a70e658dc750a8921d2
marcmdev <marcmiranda.dev@gmail.com>
2018-12-21 16:40:51 -0200

    Added CSS and the JS Code for the buttons and content

---
+++ documentation page/script.js
@@ -0,0 +1,14 @@
+const sideBarBtn = Array.from(document.querySelectorAll('.sb-btn'))
+const content = Array.from(document.querySelectorAll('.content'));
+
+function showContent() {
+
+  sideBarBtn.forEach(btn => btn.classList.remove('focus'));
+  this.classList.add('focus');
+
+  content.forEach(page => page.classList.remove('block'));
+  content[sideBarBtn.indexOf(this)].classList.add('block');
+
+}
+
+sideBarBtn.forEach(btn => btn.addEventListener('click', showContent))
